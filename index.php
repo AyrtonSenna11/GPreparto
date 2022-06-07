61e26c5f2857b597c094cb6e12b4e485b1e3d197
renz21pao <lucianorenzo487@gmail.com>
2022-06-07 11:39:14 -0500

    se agrego un indice

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reparto de Delivery</title>
+    <link rel="shortcut icon" href="img/fast.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Acerca de</a>
+            <a href="#">Portafolio</a>
+            <a href="#">Servicios</a>
+            <a href="#">Contacto</a>
+        </nav>
+        <section class="textos-header">
+            <h1>Potencia tus datos</h1>
+            <h2>Para tu pagina web</h2>
+        </section>
+        <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,50.06 C149.99,150.22 349.21,-50.06 500.00,50.06 L500.00,150.22 L0.00,150.22 Z" style="stroke: none; fill: #08f;"></path></svg></div>
+    </header>
+        <main>
+            <section class="contenedor sobre-nosotros">
+                <h2>nuestro producto</h2>
+                <div class="contenedor sobre-nosotros">
+                    <img src="img/fast.png" alt="">
+                </div>
+            </section>
+        </main>
+</body>
+</html>
